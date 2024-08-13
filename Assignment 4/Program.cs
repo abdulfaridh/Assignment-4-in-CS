56261bc448506f30229f32a6c449e093929232d2
farid <farid@LAPTOP-SPTVSQVM>
2024-08-13 20:37:45 +0530

    Add project files.

---
+++ Assignment 4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Assignment4
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string input = Console.ReadLine();
+        int wordCount =input.Length;
+        Console.WriteLine($"Number of words in the string: {wordCount}");
+    }
+}
